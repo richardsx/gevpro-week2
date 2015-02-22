a32c771e005c1b3960472c1fec8225ab51594021
Richards Zheng <r-zh3ng@hotmail.com>
2015-02-22 22:03:01 +0100

    changed country and test.py

--- country.py
+++ country.py
@@ -2,11 +2,13 @@
 #Richards Zheng
 
 
-class Country():
-	
+class country():
+		
+		# Create the class country
 		def __init__(self, string):
 			self.string = string
 		
+		# Return the string given in the terminal
 		def __str__(self):
 			return "Hello from {}".format(self.string)
 		
--- testclass.py
+++ testclass.py
@@ -2,12 +2,24 @@
 #Richards Zheng
 
 import sys
-from country import Country
+from country import country
+
 
 def main(argv):
-	n = (argv[1])
-	x = Country(n)
-	print(x)
+	if len (argv) == 3:
+		# Create country object and print
+		a = (argv[1])
+		b = (argv[2])
+		country1 = country(a)
+		country2 = country(b)
+		print(country1)
+		print(country2)
+	else:
+		print("The invocation should be: ./testclass.py ", "first country name and", "second country name")
 	
 if __name__ == "__main__":	
 	main(sys.argv)	
+
+
+
+		
